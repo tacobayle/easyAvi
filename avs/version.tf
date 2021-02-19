980efb26ccb453bf743f9dff67e859b27ef910de
tacobayle <nicolas.bayle@gmail.com>
2021-02-19 06:36:20 +0000

    20210211

--- avs/version.tf
+++ avs/version.tf
@@ -2,19 +2,19 @@ terraform {
   required_providers {
     nsxt = {
       source = "vmware/nsxt"
-      version = "v3.1.1"
+      version = "= 3.1.1"
     }
     vsphere = {
       source = "hashicorp/vsphere"
-      version = "v1.24.3"
+      version = "= 1.24.3"
     }
     template = {
       source = "hashicorp/template"
-      version = "v2.2.0"
+      version = "= 2.2.0"
     }
     null = {
       source = "hashicorp/null"
-      version = "v3.0.0"
+      version = "= 3.0.0"
     }
   }
   required_version = ">= 0.13"
