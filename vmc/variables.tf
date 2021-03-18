4d37e66932eaee2cd8e328e26e609e5a4d641639
tacobayle <nicolas.bayle@gmail.com>
2021-03-18 18:37:15 +0000

    20210318

--- vmc/variables.tf
+++ vmc/variables.tf
@@ -124,8 +124,8 @@ variable "no_access_vcenter" {
     name = "cloudVmc" # static
     environment = "vmc" # static
     dhcp_enabled = true # static
-    application = false # dynamic, from WebUI - if application is enabled
-    public_ip = false # dynamic, from WebUI - if application is enabled and public_ip is enabled
+    application = true # dynamic, from WebUI - if application is enabled
+    public_ip = true # dynamic, from WebUI - if application is enabled and public_ip is enabled
     dfw_rules = false # dynamic, from WebUI - if application is enabled and dfw_rules is enabled
     nsxt_exclusion_list = true # dynamic, from WebUI - based of nsx_exclusion or Tunnel mode
     vcenter = {
