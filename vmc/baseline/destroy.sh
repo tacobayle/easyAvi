cbb6867c6af2203b1937e7c8a41f2a869ea30e1e
tacobayle <nicolas.bayle@gmail.com>
2021-03-25 17:35:37 +0000

    20210325

---
+++ vmc/baseline/destroy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export GOVC_DATACENTER=$(cat sddc.json | jq -r .no_access_vcenter.vcenter.dc)
+export GOVC_URL=$(cat sddc.json | jq -r .vmc_vsphere_username):$(cat sddc.json | jq -r .vmc_vsphere_password)@$(cat sddc.json | jq -r .vmc_vsphere_server)
+export GOVC_INSECURE=true
+export GOVC_DATASTORE=$(cat sddc.json | jq -r .no_access_vcenter.vcenter.datastore)
+IFS=$'\n'
+for vm in $(govc find / -type m)
+do
+  if [[ $(basename $vm) == EasyAvi-se* ]]
+  then
+    echo "removing VM called $(basename $vm)"
+    govc vm.destroy $(basename $vm)
+  fi
+done
+python3 python/pyVMC3.py $(cat sddc.json | jq -r .vmc_nsx_token) $(cat sddc.json | jq -r .vmc_org_id) $(cat sddc.json | jq -r .vmc_sddc_id) remove-easyavi-rules easyavi_
+python3 python/pyVMC3.py $(cat sddc.json | jq -r .vmc_nsx_token) $(cat sddc.json | jq -r .vmc_org_id) $(cat sddc.json | jq -r .vmc_sddc_id) remove-exclude-list EasyAvi-SE
+terraform destroy -auto-approve -var-file=sddc.json -var-file=ip.json
