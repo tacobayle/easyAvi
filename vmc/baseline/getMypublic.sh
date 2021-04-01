807628c5269939b76ccd56a7163f5850b2724290
tacobayle <nicolas.bayle@gmail.com>
2021-04-01 16:35:50 +0000

    20210401

--- vmc/baseline/getMypublic.sh
+++ vmc/baseline/getMypublic.sh
@@ -2,5 +2,23 @@
 ifPrimary=$(ip route | grep default | sed -e "s/^.*dev.//" -e "s/.proto.*//")
 ip=$(ip -f inet addr show $ifPrimary | awk '/inet / {print $2}' | awk -F/ '{print $1}')
 echo $ip
-myPublicIP=$(curl ifconfig.me)
+declare -a arr=("checkip.amazonaws.com" "ifconfig.me" "ifconfig.co")
+while [ -z "$myPublicIP" ]
+do
+  for url in "${arr[@]}"
+  do
+    echo "checking public IP on $url"
+    myPublicIP=$(curl $url)
+    if [[ $myPublicIP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]
+    then
+      break
+    fi
+  done
+  if [ -z "$myPublicIP" ]
+  then
+    echo 'Failed to retrieve Public IP address' > /dev/stderr
+    /bin/false
+    exit
+  fi
+done
 echo "{\"my_private_ip\": \"$ip\", \"my_public_ip\": \"$myPublicIP\"}" | tee ip.json
